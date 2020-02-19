c140203e33b9ac074f237b1608b9e9cc03c27aed
oxygen1997 <chzeyi@126.com>
2020-02-19 17:59:16 +0800

    update SSM module.

---
+++ mybatis-spring/src/main/java/com/zy/mybatis/bean/Dept.java
@@ -0,0 +1,63 @@
+package com.zy.mybatis.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class Dept implements Serializable {
+
+    Integer id;
+    String deptName;
+    private List<Employee> employeeList;
+
+
+    public Dept() {
+    }
+
+    public Dept(Integer id) {
+        this.id = id;
+    }
+
+    public Dept(String deptName, List<Employee> employeeList) {
+        this.deptName = deptName;
+        this.employeeList = employeeList;
+    }
+
+    public Dept(Integer id, String deptName, List<Employee> employeeList) {
+        this.id = id;
+        this.deptName = deptName;
+        this.employeeList = employeeList;
+    }
+
+    @Override
+    public String toString() {
+        return "Dept{" +
+                "id=" + id +
+                ", deptName='" + deptName + '\'' +
+                '}';
+    }
+
+    public List<Employee> getEmployeeList() {
+        return employeeList;
+    }
+
+    public void setEmployeeList(List<Employee> list) {
+        this.employeeList = list;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+
+}
