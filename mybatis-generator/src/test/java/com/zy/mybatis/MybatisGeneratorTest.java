0982a855491afceab1cf097ee2aa5c6157d092a9
oxygen1997 <chzeyi@126.com>
2020-02-20 17:51:44 +0800

    update new module mybatis-generator.

---
+++ mybatis-generator/src/test/java/com/zy/mybatis/MybatisGeneratorTest.java
@@ -0,0 +1,27 @@
+package com.zy.mybatis;
+
+import org.junit.Test;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MybatisGeneratorTest {
+
+    @Test
+    public void testGenerator() throws Exception {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        File configFile = new File("src/main/resources/generator/mybatis-generator.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+                callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
