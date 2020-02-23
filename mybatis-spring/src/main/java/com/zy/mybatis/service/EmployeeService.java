c8b586216a8baff85fbe5381bb38fecad650f657
oxygen1997 <chzeyi@126.com>
2020-02-23 13:28:46 +0800

    update code for batch and mybatis-spring.

--- mybatis-spring/src/main/java/com/zy/mybatis/service/EmployeeService.java
+++ mybatis-spring/src/main/java/com/zy/mybatis/service/EmployeeService.java
@@ -1,5 +1,6 @@
 package com.zy.mybatis.service;
 
+import com.zy.mybatis.bean.Dept;
 import com.zy.mybatis.bean.Employee;
 import com.zy.mybatis.mapper.EmployeeMapper;
 import org.apache.ibatis.session.SqlSession;
@@ -7,10 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class EmployeeService {
+
+    //使用applicationContext配置文件中的配置好的SqlSession bean来自动注入使用批处理
     @Autowired
     private SqlSession sqlSession;
 
@@ -18,11 +23,21 @@ public class EmployeeService {
     EmployeeMapper employeeMapper;
     
     //批处理sql
-    public void batchInsert(){
+    public Map<String,Object> batchInsert(){
+        boolean status = false;
         EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
+        long start = System.currentTimeMillis();
         for(int i=0;i<10000;i++){
-            employeeMapper.addEmployee(new Employee("WuYanzu"+(i+1),i+1+"@163.com","1",null));
+            status =  employeeMapper.addEmployee(new Employee("WuYanzu"+(i+1),"1",
+                    i+1+"@163.com",new Dept(2,null,null)));
         }
+        long end = System.currentTimeMillis();
+        Long time = end-start;
+        System.out.println("执行时长："+time);
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("time",time);
+        map.put("status",status);
+        return map;
     }
     
     public Employee getById(Integer id){
@@ -32,6 +47,7 @@ public class EmployeeService {
     public List<Employee> getByGenderList(Employee employee){
         return employeeMapper.getByGenderList(employee);
     }
+
     public List<Employee> getAllEmployee(){
         return employeeMapper.getAllEmployee();
     }
